3333ed9f0e1896ff9cd8fa5810ab4799c8941977
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-06 00:19:41 +0530

    set javascript code

---
+++ 04_object/01_create.js
@@ -0,0 +1,13 @@
+//how to create object
+//const person={name:"rahul",age:18};
+//console.log(person);
+const person={
+    name:"rahul",
+    age:18,
+    hobbies:["cricket","football","volleyball"]
+}
+console.log(person)
+
+//how to access dat from object
+//console.log(person.name);
+//console.log(person.age);
