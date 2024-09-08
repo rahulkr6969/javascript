27660cbbfe8a53406d5878fd9f3ce393661cb188
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-09 01:45:05 +0530

    set javascript code

---
+++ 05_function/01_declaration.js
@@ -0,0 +1,10 @@
+function singhHappyBirthday(){
+    console.log("Happy birthday to you....");
+}
+ singhHappyBirthday();
+ singhHappyBirthday();
+
+ function sumTwoNumber(number1,number2){
+    console.log(number1+number2);
+ }
+    sumTwoNumber(4,5);
---
+++ 05_function/02_isEven.js
@@ -0,0 +1,28 @@
+//isEven
+function isEven(number){
+    if(number%2===0){
+        return true;
+    }
+    return false;
+}
+ console.log(isEven(6));
+
+//any String
+function firstChart(anyString){
+    return anyString[0];
+}
+console.log(firstChart("abc"));
+
+//findtarget
+
+function findTarget(array,target){
+    for(let i = 0;i<array.length; i++){
+    if(array[i]===target){
+    return i;
+  }
+ }
+ return-1;
+}
+const myArray=[1,2,8,9,11]
+const ans = findTarget(myArray,2);
+console.log(ans);
