097855ba9bdab8ed340e754de05d73bae82a45aa
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-11 21:31:14 +0530

    set javascript code

---
+++ 05_function/03_arrow.js
@@ -0,0 +1,11 @@
+//arrow function
+const singhHappyBirthday=()=>{
+    console.log("happy birthday to you....");
+}
+singhHappyBirthday();
+//sumthreenumber
+const sumThreeNumber=(number1,number2,number3)=>{
+    return number1+number2+number3;
+}
+const ans=sumThreeNumber(2,5,4);
+console.log(ans);
