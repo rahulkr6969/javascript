01dc016db8d0d41937b74790425a8ce62de2f8ae
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-13 21:22:45 +0530

    set javascript code

---
+++ 06_method/01_forEach.js
@@ -0,0 +1,9 @@
+//forEach method
+const numbers=[2,5,4,6,7];
+
+// function myFunc(number,index){
+//     console.log(`index is ${index} number is ${number}`);
+// }
+ numbers.forEach(function(number,index){
+    console.log(`index is ${index} number is ${number}`);
+ });
