9deeb1489a3f9ca8fe0d09474de4c9ed31816a04
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-16 00:49:24 +0530

    set javascript code

--- 06_method/03_filter.js
+++ 06_method/03_filter.js
@@ -1,7 +1,7 @@
 //filter method
 const numbers=[1,3,2,6,4,8];
-const isEven =function(number){
-    return number%2==0;
+const isOdd =function(number){
+    return number%2!==0;
 }
-const evenNumber=numbers.filter(isEven);
-console.log(evenNumber);
+const oddNumber=numbers.filter(isOdd);
+console.log(oddNumber);
