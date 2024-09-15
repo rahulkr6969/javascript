a0423fe352df77d66c89c1459f76e9d1588d3be0
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-15 23:22:07 +0530

    set javascript code

--- 06_method/01_forEach.js
+++ 06_method/01_forEach.js
@@ -1,9 +1,22 @@
 //forEach method
-const numbers=[2,5,4,6,7];
+//const numbers=[2,5,4,6,7];
 
 // function myFunc(number,index){
 //     console.log(`index is ${index} number is ${number}`);
-// }
- numbers.forEach(function(number,index){
-    console.log(`index is ${index} number is ${number}`);
- });
+// }  
+ //numbers.forEach(function(number,index){
+   // console.log(`index is ${index} number is ${number}`);
+ //});
+
+ const users=[
+   {firstName:"rahul", age:22},
+   {firstName:"mohit", age:21},
+   {firstName:"sagar", age:20},
+   {firstName:"kunal", age:19},
+ ]
+//    users.forEach(function(user){
+//     console.log(user.firstName)
+//   });
+for(let user of users){
+   console.log(user);
+}
---
+++ 06_method/02_map.js
@@ -0,0 +1,8 @@
+//map method
+
+const numbers=[3,4,5,6,8];
+const square=function(number){
+    return number*number;
+}
+const squareNumber=numbers.map(square);
+console.log(squareNumber);
