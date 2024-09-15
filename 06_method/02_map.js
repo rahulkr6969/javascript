54084bdcc811dd32f5beee2b880fffc4054979dd
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-16 00:21:58 +0530

    set javascript code

--- 06_method/02_map.js
+++ 06_method/02_map.js
@@ -1,7 +1,9 @@
 //map method
 const numbers=[3,4,5,6,8];
-const square=function(number){
+// const square=function(number){
+//     return number*number;
+//}
+const squareNumber=numbers.map(function(number){
     return number*number;
-}
-const squareNumber=numbers.map(square);
+});
 console.log(squareNumber);
