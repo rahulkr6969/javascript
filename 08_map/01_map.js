c867e13812d53f4350b4cc47ca8576d440c82ec2
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-18 23:46:13 +0530

    set javascript code

---
+++ 08_map/01_map.js
@@ -0,0 +1,15 @@
+//Maps
+//map is iterable
+//stored data in oredered fashion
+//store key value pair(like object)
+//duplicate keys are not allowed like objects
+//different between maps and objects 
+//objects can only havestring or symbol
+//as key
+//in maps you can useanything as key
+//like array, number, string
+const person={
+    firstName:"rahul",
+    age:18,
+}
+console.log(person.firstName);
