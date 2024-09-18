67f81b9f71a410799eb2c918a52660126507f1a1
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-18 23:27:39 +0530

    set javascript code

--- 07_set/01_set.js
+++ 07_set/01_set.js
@@ -4,5 +4,23 @@
 //no-index based access
 //order is not gurranted
 //unique items only(no duplicates allowed)
-const numbers = new Set([1,2,3,4]);
-console.log(numbers);
+const numbers = new Set();
+numbers.add(1);
+numbers.add(2);
+numbers.add(3);
+numbers.add(4);
+numbers.add(5);
+numbers.add(6);
+// if(numbers.has(1)){
+//     console.log("1 has present");
+// }else{
+//     console.log("1 has not present")
+// }
+// console.log(numbers);
+// for(number of numbers){
+//     console.log(number);
+// }
+
+const myArray=[1,2,2,3,4,4,5,5,6];
+const uniqueElements=new Set(myArray);
+console.log(uniqueElements.length);
