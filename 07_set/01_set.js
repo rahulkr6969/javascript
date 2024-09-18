e87c1099d9678d662ac9400c30f05c8625550982
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-18 23:09:07 +0530

    set javascript code

---
+++ 07_set/01_set.js
@@ -0,0 +1,8 @@
+//Set(it is iterable)
+//store data
+//Sets also have its own methods
+//no-index based access
+//order is not gurranted
+//unique items only(no duplicates allowed)
+const numbers = new Set([1,2,3,4]);
+console.log(numbers);
