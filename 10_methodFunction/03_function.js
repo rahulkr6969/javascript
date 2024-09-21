cfe79b8d812d2dab76c386d7a01f88a8b7d4e66e
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-22 00:47:40 +0530

    set javascript code

---
+++ 10_methodFunction/03_function.js
@@ -0,0 +1,12 @@
+//function(that function create object)
+// 2.)add key value pair
+// 3.)object ko return karega
+function createUser(firstName, lastName, email, age, address){
+    const user={};
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+
+}
