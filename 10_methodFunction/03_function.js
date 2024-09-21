366ea982c6e5fef2237e8ded21945c6754314dfc
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-22 03:16:29 +0530

    set javascript code

--- 10_methodFunction/03_function.js
+++ 10_methodFunction/03_function.js
@@ -8,23 +8,25 @@ const userMethods={
     },
     is18:function(){
         return this.age >=18;
-  }
+    },
+    sing:function(){
+        return 'too na na na naa';
+    }
 }
 function createUser(firstName, lastName, email, age, address){
-    const user={};
+    const user= Object.create(userMethods);
     user.firstName = firstName;
     user.lastName = lastName;
     user.email = email;
     user.age = age;
     user.address = address;
-    user.about = userMethods.about;
-    user.is18 = userMethods.is18;
     
     return user;
 }
 const user1=createUser("rahul","kumar","rahul.kumar6969@gmail.com",'18',"Bajrangpuri");
 const user2=createUser("mohit","kumar","mohit.kumar@gmail.com",'17',"Bajrangpuri");
 const user3=createUser("sumit","kumar","sumit.kumar66@gmail.com",'16',"Bajrangpuri");
-console.log(user2);
-console.log(user3.about());
+console.log(user1);
+//console.log(user3.about());
+console.log(user1.about());
 
