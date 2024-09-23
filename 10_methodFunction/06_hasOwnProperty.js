c5d152f5a88383b9c4283cb88ff709631791caa2
rahul kumar <rahul.kumar6969rk@gmail.com>
2024-09-23 23:54:13 +0530

    set javascript code

--- 10_methodFunction/06_hasOwnProperty.js
+++ 10_methodFunction/06_hasOwnProperty.js
@@ -1,5 +1,5 @@
 //hasOwnProperty
-function createUser(firstName,lastName,age,email,address){
+function CreateUser(firstName,lastName,age,email,address){
 
     this.firstName = firstName;
     this.lastName = lastName;
@@ -8,17 +8,17 @@ function createUser(firstName,lastName,age,email,address){
     this.address = address;
     
 }
-createUser.prototype.about = function(){
+CreateUser.prototype.about = function(){
     return `${this.firstName} is ${this.age} years old.`
 };
-createUser.prototype.is18 = function(){
+CreateUser.prototype.is18 = function(){
     return this.age>=18
 };
-createUser.prototype.sing = function(){
+CreateUser.prototype.sing = function(){
     return "la la la naaa"
 };
-const user1 = new createUser("rahul","kumar",'18',"rahul.kumar6969@gmail.com","Bajrangpuri");
-const user2 = new createUser("sumit","kumar",'19',"sumit.kumar1234@gmail.com","ranchi");
-const user3 = new createUser("mohit","kumar",'20',"mohit.kumar69@gmail.com","odisha");
+const user1 = new CreateUser("rahul","kumar",'18',"rahul.kumar6969@gmail.com","Bajrangpuri");
+const user2 = new CreateUser("sumit","kumar",'19',"sumit.kumar1234@gmail.com","ranchi");
+const user3 = new CreateUser("mohit","kumar",'20',"mohit.kumar69@gmail.com","odisha");
 console.log(user1);
 console.log(user3.about());
