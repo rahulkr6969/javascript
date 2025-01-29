267c5cc9bb81bcceaaff993433b10f2742ce14d0
rahul kumar <rahul.kumar6969rk@gmail.com>
2025-01-29 14:27:18 +0530

    set javascript code

---
+++ 12_domTutorial/102.js
@@ -0,0 +1,7 @@
+//select element using get element by Id
+// const mainHeading=document.getElementById("main-heading");
+// console.log(mainHeading);
+
+//select element using query selector
+const mainHeading=document.querySelector("#main-heading");
+console.log(mainHeading);
